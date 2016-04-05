1cd8cd1d18d53547689c401f8f315d92904b0fa7
mattiewae <mathiaswaegemans@gmail.com>
2016-04-05 17:44:22 +0200

    Added files via upload

---
+++ ClientUpdateScript.ps1
@@ -0,0 +1,21 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+#Set-Location -Path C:\Users\ENG\Downloads
+Set-Location -Path C:\Users\Mathias\Downloads
+
+wget https://raw.githubusercontent.com/mattiewae/test/master/winupdate.ps1 -OutFile winupdate.ps1
+
+$ChkFile = "C:\Users\Mathias\Downloads\winupdate.ps1" 
+$FileExists = Test-Path $ChkFile 
+If ($FileExists -eq $True) {
+Write-Host "File download success"
+Invoke-Expression C:\Users\Mathias\Downloads\winupdate.ps1
+
+}
+
+Remove-Item C:\Users\ENG\Downloads\winupdate.ps1
+
+
+
+
+
