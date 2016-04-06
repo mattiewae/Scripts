dd5f5c559a44b74f3791271b06df1b1cd1ec2c47
mattiewae <mathiaswaegemans@gmail.com>
2016-04-06 12:46:39 +0200

    Remove script

--- ClientUpdateScript.ps1
+++ ClientUpdateScript.ps1
@@ -3,7 +3,7 @@
 Set-Location -Path $home\Downloads
 
 wget https://raw.githubusercontent.com/mattiewae/test/master/winupdate.ps1 -OutFile winupdate.ps1
-wget https://github.com/mattiewae/test/raw/master/Test.zip -OutFile test.zip
+
 
 $ChkFile = "$home\Downloads\winupdate.ps1" 
 $FileExists = Test-Path $ChkFile 
@@ -12,5 +12,5 @@ Write-Host "File download success"
 Invoke-Expression $home\Downloads\winupdate.ps1 | Out-Null
 }
 
-Write-Host "Update file removed"
 Remove-Item $home\Downloads\winupdate.ps1
+Write-Host "Update file removed"
