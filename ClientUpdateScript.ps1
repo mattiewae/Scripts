70e44570d160615937a12bf55c1adc287b0cd84b
mattiewae <mathiaswaegemans@gmail.com>
2016-04-08 17:00:59 +0200

    update

--- ClientUpdateScript.ps1
+++ ClientUpdateScript.ps1
@@ -1,14 +1,18 @@
 ﻿Set-ExecutionPolicy RemoteSigned
 
 
-
 if ($PSVersionTable.PSVersion.Major -eq 5){
     
     Set-Location -Path $home\Downloads
 
     iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/winupdate.ps1'))
-    
-    #$ChkFile = "$home\Downloads\winupdate.ps1" 
+}
+else{
+    iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/installPSv5.ps1'))
+}
+
+
+#$ChkFile = "$home\Downloads\winupdate.ps1" 
     #$FileExists = Test-Path $ChkFile 
     #If ($FileExists -eq $True) {
     #Write-Host "File download success"
@@ -19,7 +23,3 @@ if ($PSVersionTable.PSVersion.Major -eq 5){
 
     #Remove-Item $home\Downloads\winupdate.ps1
     #Write-Host "Update file removed"
-}
-else{
-    iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/installPSv5.ps1'))
-}
