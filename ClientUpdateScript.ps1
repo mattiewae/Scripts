6ff3939e491c91c1fe53e8dc09740dee64da4043
mattiewae <mathiaswaegemans@gmail.com>
2016-04-08 14:43:19 +0200

    Update ClientUpdateScript.ps1

--- ClientUpdateScript.ps1
+++ ClientUpdateScript.ps1
@@ -8,11 +8,11 @@ if ($PSVersionTable.PSVersion.Major -eq 5){
 
     iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/winupdate.ps1'))
     
-    $ChkFile = "$home\Downloads\winupdate.ps1" 
-    $FileExists = Test-Path $ChkFile 
-    If ($FileExists -eq $True) {
-    Write-Host "File download success"
-    Invoke-Expression $home\Downloads\winupdate.ps1 | Out-Null
+    #$ChkFile = "$home\Downloads\winupdate.ps1" 
+    #$FileExists = Test-Path $ChkFile 
+    #If ($FileExists -eq $True) {
+    #Write-Host "File download success"
+    #Invoke-Expression $home\Downloads\winupdate.ps1 | Out-Null
     }
     
     #Remove-Item $home\Downloads\winupdate.ps1
