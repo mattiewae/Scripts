63717559f1ba8a93ae050e3ff2cd4e2c17e1f1b5
mattiewae <mathiaswaegemans@gmail.com>
2016-04-08 09:37:52 +0200

    Client update script
    if else

--- ClientUpdateScript.ps1
+++ ClientUpdateScript.ps1
@@ -1,30 +1,31 @@
 ﻿Set-ExecutionPolicy RemoteSigned
 
+
+
 if ($PSVersionTable.PSVersion.Major -eq 5){
-    Write-Host "PS Version 5.0"
+    
+    Set-Location -Path $home\Downloads
+
+    iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/winupdate.ps1'))
+    
+    $ChkFile = "$home\Downloads\winupdate.ps1" 
+    $FileExists = Test-Path $ChkFile 
+    If ($FileExists -eq $True) {
+    Write-Host "File download success"
+    Invoke-Expression $home\Downloads\winupdate.ps1 | Out-Null
+    }
+    
+    Remove-Item $home\Downloads\winupdate.ps1
+    Write-Host "Update file removed"
 }
-else {
+else{
     ######################################################
-    # Install PS V5.0
-    # Reboot and run script again.
+    Write-Host Install PS V5.0
+    Write-Host Reboot and run script again.
     ######################################################
     Write-Host "Installing Chocolatey"
     iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
     Write-Host
     cinst powershell -pre -y
     cinst upgrade powershell -pre -y
-    }
-
-Set-Location -Path $home\Downloads
-
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/winupdate.ps1'))
-
-$ChkFile = "$home\Downloads\winupdate.ps1" 
-$FileExists = Test-Path $ChkFile 
-If ($FileExists -eq $True) {
-Write-Host "File download success"
-Invoke-Expression $home\Downloads\winupdate.ps1 | Out-Null
 }
-
-Remove-Item $home\Downloads\winupdate.ps1
-Write-Host "Update file removed"
