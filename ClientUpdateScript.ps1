839a99ff708b021d3bd725bbe7f23be2b3cb6a95
mattiewae <mathiaswaegemans@gmail.com>
2016-04-08 13:52:09 +0200

    Update ClientUpdateScript.ps1

--- ClientUpdateScript.ps1
+++ ClientUpdateScript.ps1
@@ -26,7 +26,7 @@ else{
     Write-Host "Installing Chocolatey"
     iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
     Write-Host
-    choco install dotnet4.6
+    choco install dotnet4.6 -y
     cinst boxstarter -y
     cinst powershell -pre -y
     cinst upgrade powershell -pre -y
