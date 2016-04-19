f621ddbb2220806e117b5439383ca2ff219c3f9d
mattiewae <mathiaswaegemans@gmail.com>
2016-04-19 12:56:02 +0200

    nuttige dingen

---
+++ nuttigedingen.ps1
@@ -0,0 +1,16 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+Set-Location -Path $home\Downloads
+wget https://github.com/mattiewae/test/raw/master/nuttige.zip -OutFile .\nuttige.zip
+
+Write-Host "Download allerlei nuttige dingen succes"
+Expand-Archive -Path nuttige.zip -DestinationPath ND -Force
+Write-Host "files unzipped"
+
+Copy-Item '.\ND\Allerlei nuttige dingen'  "$home\Desktop\" -Recurse
+
+
+Remove-Item $home\Downloads\ND -Recurse
+Write-Host "Remove ND"
+Remove-Item $home\Downloads\nuttige.zip 
+Write-Host "Remove ZIP"
