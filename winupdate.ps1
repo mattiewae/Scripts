120635400facd58665100aadd6d481b0ed012a41
mattiewae <mathiaswaegemans@gmail.com>
2016-04-19 13:23:44 +0200

    test

--- winupdate.ps1
+++ winupdate.ps1
@@ -14,6 +14,11 @@ $FileExists = Test-Path $ChkFile
         Write-Host
     }
 
+
+
+Write-Host "update all apps"
+cup all -y
+
 Write-Host "Installing applications from Chocolatey"
 
 cinst wget -y
@@ -30,15 +35,13 @@ cinst windirstat -y
 cinst quicktime -y 
 cinst irfanview -y
 
-Write-Host "update all apps"
-cup all
-
-Write-Host "Install windows updates - critical only"
-Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
-
+Write-Host "Update OT's"
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/otupdate.ps1'))
 
 Write-Host "Update OT's"
-Set-Location -Path $home\Downloads
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/otupdate.ps1'))
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/nuttigedingen.ps1'))
+
+Write-Host "Install windows updates - critical only"
+#Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
 
 Restart-Computer
