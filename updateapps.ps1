c6f721b8df9a0cb9cde68640718b39da935889fc
mattiewae <mathiaswaegemans@gmail.com>
2016-04-20 13:08:01 +0200

    updates
    update in aparte file

---
+++ updateapps.ps1
@@ -0,0 +1,32 @@
+﻿$ChkFile = "C:\ProgramData\chocolatey\choco.exe" 
+$FileExists = Test-Path $ChkFile 
+    If ($FileExists -eq $True) {
+        Write-Host "Choco already installed"
+    }
+    else {
+        ######################################################
+        # Install apps using Chocolatey
+        ######################################################
+        Write-Host "Installing Chocolatey"
+        iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+        Write-Host
+    }
+
+Write-Host "Installing applications from Chocolatey"
+
+cinst wget -y
+cinst teamviewer8 -y
+cinst flashplayerplugin -y
+cinst googlechromex64 -y 
+cinst firefox -y 
+cinst notepadplusplus.install -y
+cinst jre8 -y 
+cinst vlc -y
+cinst ccleaner -y
+cinst adobeair -y
+cinst windirstat -y
+cinst quicktime -y 
+cinst irfanview -y
+
+Write-Host "update all apps"
+cup all -y
--- winupdate.ps1
+++ winupdate.ps1
@@ -1,39 +1,8 @@
 ﻿Set-ExecutionPolicy RemoteSigned
 
 
-$ChkFile = "C:\ProgramData\chocolatey\choco.exe" 
-$FileExists = Test-Path $ChkFile 
-    If ($FileExists -eq $True) {
-        Write-Host "Choco already installed"
-    }
-    else {
-        ######################################################
-        # Install apps using Chocolatey
-        ######################################################
-        Write-Host "Installing Chocolatey"
-        iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
-        Write-Host
-    }
-
-Write-Host "Installing applications from Chocolatey"
-
-cinst wget -y
-cinst teamviewer8 -y
-cinst flashplayerplugin -y
-cinst googlechromex64 -y 
-cinst firefox -y 
-cinst notepadplusplus.install -y
-cinst jre8 -y 
-cinst vlc -y
-cinst ccleaner -y
-cinst adobeair -y
-cinst windirstat -y
-cinst quicktime -y 
-cinst irfanview -y
-
-
-Write-Host "update all apps"
-cup all -y
+Write-Host "Update apps"
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/updateapps.ps1'))
 
 Write-Host "Update OT's"
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/otupdate.ps1'))
