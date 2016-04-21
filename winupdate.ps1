32dee0f777d563e886b2fba06163fe45fbc4c071
mattiewae <mathiaswaegemans@gmail.com>
2016-04-21 16:16:05 +0200

    test

--- winupdate.ps1
+++ winupdate.ps1
@@ -11,6 +11,6 @@ Write-Host "Update nuttige dingen"
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattiewae/test/master/nuttigedingen.ps1'))
 
 Write-Host "Install windows updates - critical only"
-#Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
+Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
 
 Restart-Computer
