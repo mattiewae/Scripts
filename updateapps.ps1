cff694ba8897af6b4a4bddf1c7a82e0bb0bfaf6d
mattiewae <mathiaswaegemans@gmail.com>
2016-10-27 11:45:24 +0200

    Update updateapps.ps1
    Enkel Cup all -y, rest in commentaar

--- updateapps.ps1
+++ updateapps.ps1
@@ -1,36 +1,36 @@
-﻿$ChkFile = "C:\ProgramData\chocolatey\choco.exe" 
-$FileExists = Test-Path $ChkFile 
-    If ($FileExists -eq $True) {
-        Write-Host "Choco already installed"
-    }
-    else {
-        ######################################################
-        # Install apps using Chocolatey
-        ######################################################
-        Write-Host "Installing Chocolatey"
-        iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
-        Write-Host
-    }
-
-Write-Host "Installing applications from Chocolatey"
-
-cinst wget -y
-cinst teamviewer8 -y
-cinst flashplayerplugin -y
-cinst googlechrome -y 
-cinst firefox -y 
-cinst notepadplusplus.install -y
-cinst jre8 -y 
-cinst jdk8 -y
-cinst vlc -y
-cinst ccleaner -y
-cinst adobeair -y
-cinst windirstat -y
-cinst quicktime -y 
-cinst irfanview -y
-cinst libreoffice -y
-cinst winff -y
-cinst mediainfo -y
+#$ChkFile = "C:\ProgramData\chocolatey\choco.exe" 
+#$FileExists = Test-Path $ChkFile 
+#    If ($FileExists -eq $True) {
+#        Write-Host "Choco already installed"
+#    }
+#    else {
+#        ######################################################
+#        # Install apps using Chocolatey
+#        ######################################################
+#        Write-Host "Installing Chocolatey"
+#        iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+#        Write-Host
+#    }
+#
+#Write-Host "Installing applications from Chocolatey"
+#
+#cinst wget -y
+#cinst teamviewer8 -y
+#cinst flashplayerplugin -y
+#cinst googlechrome -y 
+#cinst firefox -y 
+#cinst notepadplusplus.install -y
+#cinst jre8 -y 
+#cinst jdk8 -y
+#cinst vlc -y
+#cinst ccleaner -y
+#cinst adobeair -y
+#cinst windirstat -y
+#cinst quicktime -y 
+#cinst irfanview -y
+#cinst libreoffice -y
+#cinst winff -y
+#cinst mediainfo -y
 
 Write-Host "update all apps"
 cup all -y
